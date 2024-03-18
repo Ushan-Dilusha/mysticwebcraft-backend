2c640a57267d84e02a0596aa28c4556bae942cc4
ushan-dilusha <ushandilusha10@gmail.com>
2024-03-18 10:10:10 +0530

    fixed backend error

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
-const express = require("express")
-const bodyParser = require('body-parser');
-const routes = require("./routes/Route")
-const cors = require("cors")
-require("dotenv").config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
 const connectDB = require("./configuration/database");
-const app = express()
-const PORT = process.env.PORT
 
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 
-app.use(bodyParser.json({ limit: '10mb' }));
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
-app.use(express.json())
-app.use(cors())
-
+const PORT = process.env.PORT || 9000;
 
 connectDB().then(() => {
     app.listen(PORT, () => {
-        console.log(`app listening at http://localhost:${PORT}`);
+        console.log(`backend running at http://localhost:${PORT}`);
     });
-}
+    }
 );
-
-app.use("/api", routes)
-app.listen(PORT, () => console.log("Listening at " + PORT))
