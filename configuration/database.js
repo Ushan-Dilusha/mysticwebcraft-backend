330b0cf213eb0051e108a6baed93578f6e7357d3
ushan-dilusha <ushandilusha10@gmail.com>
2024-03-19 23:37:21 +0530

    communities basic crud

--- configuration/database.js
+++ configuration/database.js
@@ -1,16 +1,11 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-const connectDB = async () => {
-    try {
-        if (!process.env.MONGO_URI) {
-            throw new Error("MONGO_URI environment variable is not defined.");
-        }
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB connected : ${conn.connection.host}`);
-    } catch (error) {
-        console.error("MongoDB connection error:", error.message);
-        process.exit(1);
-    }
-};
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log('MongoDB Connected...'))
+    .catch((err) => console.error('MongoDB Connection Error:', err));
 
-module.exports = connectDB;
+module.exports = mongoose.connection;
